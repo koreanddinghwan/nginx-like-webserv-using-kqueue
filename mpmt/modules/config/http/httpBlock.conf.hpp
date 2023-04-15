1bd2ac54f4ede0d8a8a334448517d0ccff67a862
myukang <myunghwan0421@gmail.com>
2023-04-15 22:12:03 +0900

    refactor(move files): commit

---
+++ mpmt/modules/config/http/httpBlock.conf.hpp
@@ -0,0 +1,21 @@
+#ifndef HTTPBLOCK_HPP
+# define HTTPBLOCK_HPP
+
+#include "../../../interface/IBlock.hpp"
+#include <fstream>
+
+class httpBlock: public IBlock
+{
+public:
+	httpBlock(std::ifstream &File) {
+		this->parse();
+	}
+
+	void parse() {}
+
+
+
+	~httpBlock() {}
+};
+
+#endif
