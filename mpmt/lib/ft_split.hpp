0000ed08e365e1fc5d8b89639d549b1213db5f98
myukang <myunghwan0421@gmail.com>
2023-05-15 03:37:48 +0900

    feat(split for remove semi): commit

--- mpmt/lib/ft_split.hpp
+++ mpmt/lib/ft_split.hpp
@@ -16,11 +16,13 @@ typedef typename std::vector<std::string> splited;
 
 	std::vector<std::string> get();
 	std::vector<std::string> split(char const *s, char c);
+	std::vector<std::string> splitRemoveSemiColon(char const *s, char c);
 	ft_split();
 	~ft_split();
 
 private:
 	void makeWord(int p_index, char const *s, char c);
+	void makeWordRemoveSemiColon(int p_index, char const *s, char c);
 	int	ft_wd_cnt(char *str, char c);
 };
 
