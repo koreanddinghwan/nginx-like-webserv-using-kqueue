68b286ae6edfa672c96c744a7510906e171c64e7
myukang <myunghwan0421@gmail.com>
2023-05-15 03:37:48 +0900

    feat(split for remove semi): commit

--- mpmt/lib/ft_split.cpp
+++ mpmt/lib/ft_split.cpp
@@ -20,6 +20,8 @@ std::vector<std::string> ft_split::split(char const *s, char c)
 	return (p);
 }
 
+
+
 std::vector<std::string> ft_split::get()
 {
 	return this->p;
@@ -40,6 +42,17 @@ void ft_split::makeWord(int p_index, char const *s, char c)
 	p.push_back(str);
 }
 
+void ft_split::makeWordRemoveSemiColon(int p_index, char const *s, char c)
+{
+	int i = 0;
+	std::string str;
+
+	while (s[i] && s[i] != c && s[i] != ';')
+		str.push_back(s[i++]);
+	p.push_back(str);
+}
+
+
 int	ft_split::ft_wd_cnt(char *str, char c)
 {
 	int	count;
@@ -59,4 +72,25 @@ int	ft_split::ft_wd_cnt(char *str, char c)
 		}
 	}
 	return (count);
+}
+
+
+std::vector<std::string> ft_split::splitRemoveSemiColon(char const *s, char c)
+{
+	for (std::vector<std::string>::iterator it = p.begin(); it != p.end(); it++)
+		(*it).clear();
+	int		p_index;
+
+	p.reserve(ft_wd_cnt((char *)s, c));
+	p_index = 0;
+	while (*s)
+	{
+		while (*s && (*s == c))
+			s++;
+		if (*s != 0)
+			makeWordRemoveSemiColon(p_index++, s, c);
+		while (*s && !(*s == c))
+			s++;
+	}
+	return (p);
 }
