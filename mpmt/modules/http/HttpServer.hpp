c778498194bcd0701f9088bb431f3b71ed51e767
myukang <myunghwan0421@gmail.com>
2023-05-17 01:02:03 +0900

    refactor(httpServer): pret

--- mpmt/modules/http/HttpServer.hpp
+++ mpmt/modules/http/HttpServer.hpp
@@ -1,36 +1,34 @@
-#ifndef HTTP_HPP
-# define HTTP_HPP
+#ifndef HTTPSERVER_HPP
+#define HTTPSERVER_HPP
 
-#include <netinet/tcp.h>
-#include <stdexcept>
-#include <sys/event.h>
-#include <iostream>
-#include <arpa/inet.h>
-#include <fcntl.h>
 #include "../../interface/IHandler.hpp"
 #include "../../interface/IHttpBlock.hpp"
 #include "../../interface/IServer.hpp"
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <iostream>
+#include <netinet/tcp.h>
+#include <stdexcept>
+#include <sys/event.h>
 
-class HttpServer : public IServer
-{
+class HttpServer : public IServer {
 private:
-	IHttpBlock *HttpBlock;
-
+  IHttpBlock *HttpBlock;
 
 public:
-	/**
-	 * @brief singleTon http Server입니다.
-	 *
-	 * @return static에 선언되 Httpserver를 리턴합니다.
-	 */
-	static HttpServer& getInstance();
+  /**
+   * @brief singleTon http Server입니다.
+   *
+   * @return static에 선언되 Httpserver를 리턴합니다.
+   */
+  static HttpServer &getInstance();
 
-	/**
-	 * @brief http config기반으로 http socket을 설정합니다.
-	 *
-	 * @throw std::runtime_error
-	 */
-	void initSocket() throw(std::runtime_error); 
+  /**
+   * @brief http config기반으로 http socket을 설정합니다.
+   *
+   * @throw std::runtime_error
+   */
+  void initSocket() throw(std::runtime_error);
 };
 
 #endif
