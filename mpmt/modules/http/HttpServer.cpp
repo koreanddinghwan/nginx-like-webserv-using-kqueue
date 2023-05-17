bfc995c6ef86b6081d967180c6f33a0594051209
koreanddinghwan <myunghwan0421@gmail.com>
2023-05-17 22:06:58 +0900

    feat(httpserver): commit

--- mpmt/modules/http/HttpServer.cpp
+++ mpmt/modules/http/HttpServer.cpp
@@ -1,4 +1,5 @@
 #include "HttpServer.hpp"
+#include <netinet/in.h>
 
 
 HttpServer& HttpServer::getInstance()
@@ -9,8 +10,85 @@ HttpServer& HttpServer::getInstance()
 
 void HttpServer::initSocket() throw(std::runtime_error)
 {
+	/* get generalBlock*/
+	G = static_cast<GeneralBlock *>(Config::getInstance().getGeneralBlock());
 	
+	/* print general data*/
+	std::cout<<"Worker_process"<<static_cast<GeneralBlock::generalConfig *>(G->getConfigData())->worker_processes<<std::endl;
 
+	/* get eventBlock*/
+	E = static_cast<EventBlock *>(Config::getInstance().getEventBlock());
+	std::cout<<"Worker_connections"<<static_cast<EventBlock::eventConfig *>(E->getConfigData())->worker_connections<<std::endl;
 
+	/* get httpBlock*/
+	H = static_cast<HttpBlock *>(Config::getInstance().getHTTPBlock());
+
+	/* get httpServerData */
+	std::cout<<"httpServerBlock"<<std::endl;
+	std::vector<IHttpBlock *> hsb = static_cast<HttpData *>(H->getConfigData())->getServerBlock();
+
+	for (int i = 0; i <  hsb.size(); i++)
+	{
+		HttpServerData *hsd = static_cast<HttpServerData *>(hsb[i]->getConfigData());
+		hsd->printServerDataConfig();
+
+		this->socketMap.insert(getSocketMapPair(hsd));
+
+		/* for (int j = 0; j < hsd->getHttpLocationBlock().size(); j++) */
+		/* { */
+		/* 	static_cast<HttpLocationData *>(hsd->getHttpLocationBlock()[j]->getConfigData())->printLocationData(); */
+		/* } */
+	}
 }
 
+HttpServer::socketMapPair HttpServer::getSocketMapPair(HttpServerData *hsd)
+{
+	socketMapPair pair;
+	int server_fd;
+	struct sockaddr_in server_addr;
+
+
+	/* create socket */
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		throw(std::runtime_error("socket error"));
+
+
+	/* set socket option */
+	int opt = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+		throw(std::runtime_error("setsockopt error"));
+
+
+	int keep_alive = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_KEEPALIVE, &keep_alive, sizeof(keep_alive)) == -1)
+		throw(std::runtime_error("setsockopt error"));
+
+
+	// Disable Nagle algorithm
+	int nagle_off = static_cast<HttpData* >(this->H->getConfigData())->getTcpNoDelay();
+	if (setsockopt(server_fd, IPPROTO_TCP, TCP_NODELAY, &nagle_off, sizeof(nagle_off)) == -1) 
+		throw(std::runtime_error("setsockopt error"));
+
+	/* set server_addr */
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(hsd->getListen());
+
+	/* bind */
+	if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+		throw(std::runtime_error("bind error"));
+
+
+	/* set server_fd to non-blocking */
+	int flags = fcntl(server_fd, F_GETFL, 0);
+	fcntl(server_fd, F_SETFL, flags | O_NONBLOCK);
+
+
+	/* listen */
+	if (listen(server_fd, 10) == -1)
+		throw(std::runtime_error("listen error"));
+
+	pair.first = server_fd;
+	pair.second = hsd;
+	return pair;
+}
--- mpmt/modules/http/HttpServer.hpp
+++ mpmt/modules/http/HttpServer.hpp
@@ -1,19 +1,36 @@
 #ifndef HTTPSERVER_HPP
 #define HTTPSERVER_HPP
 
+#include "../config/Config.hpp"
+#include "../config/http/HttpBlock.conf.hpp"
+#include "../config/GeneralBlock.conf.hpp"
+#include "../config/EventBlock.conf.hpp"
 #include "../../interface/IHandler.hpp"
-#include "../../interface/IHttpBlock.hpp"
 #include "../../interface/IServer.hpp"
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <iostream>
+#include <map>
 #include <netinet/tcp.h>
 #include <stdexcept>
 #include <sys/event.h>
 
 class HttpServer : public IServer {
 private:
-  IHttpBlock *HttpBlock;
+	GeneralBlock *G;
+	EventBlock *E;
+	HttpBlock *H;
+
+	typedef std::pair<int, HttpServerData *> socketMapPair;
+	typedef std::map<int, HttpServerData *>::iterator socketMapIter;
+	typedef std::map<int, HttpServerData *>::const_iterator socketMapConstIter;
+
+
+	/*
+	 * socket_fd : socket file descriptor for this server
+	 * locationDatas : location data for this server
+	 * */
+	std::map<int, HttpServerData *> socketMap;
 
 public:
   /**
@@ -29,6 +46,16 @@ public:
    * @throw std::runtime_error
    */
   void initSocket() throw(std::runtime_error);
+
+
+private:
+  HttpServer() {}
+  ~HttpServer() {}
+  HttpServer(const HttpServer &hs);
+  HttpServer &operator=(const HttpServer &hs);
+
+
+  socketMapPair getSocketMapPair(HttpServerData *hsd);
 };
 
 #endif
