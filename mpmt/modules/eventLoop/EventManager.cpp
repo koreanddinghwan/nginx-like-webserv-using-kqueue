e3904a5f12459b8b88c53f7ba88affe3f9c40ea9
myukang <myunghwan0421@gmail.com>
2023-05-19 20:42:34 +0900

    feat(event manager): commit

---
+++ mpmt/modules/eventLoop/EventManager.cpp
@@ -0,0 +1,41 @@
+#include "EventManager.hpp"
+#include "Event.hpp"
+
+EventManager::EventManager()
+{}
+
+EventManager::~EventManager()
+{}
+
+EventManager& EventManager::getInstance()
+{
+	static EventManager instance;
+	return instance;
+}
+
+void EventManager::addEvent(Event *e)
+{
+	this->eventMap.insert(std::pair<int, Event *>(e->getFd(), e));
+}
+
+void EventManager::deleteEvent(int fd)
+{
+	std::map<int, Event *>::iterator it = this->eventMap.find(fd);
+	if (it != this->eventMap.end())
+	{
+		delete it->second;
+		this->eventMap.erase(fd);
+	}
+}
+
+bool EventManager::isClientSocketExists(int fd)
+{
+	if (this->eventMap.find(fd) == this->eventMap.end())
+		return false;
+	return true;
+}
+
+Event *EventManager::getEvent(int fd)
+{
+	return this->eventMap[fd];
+}
