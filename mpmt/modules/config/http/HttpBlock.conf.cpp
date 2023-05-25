5418d27fa17188784ebe74b32b9da562547764eb
myukang <myunghwan0421@gmail.com>
2023-05-25 17:19:47 +0900

    feat(default server data): set deafult server data

--- mpmt/modules/config/http/HttpBlock.conf.cpp
+++ mpmt/modules/config/http/HttpBlock.conf.cpp
@@ -45,11 +45,11 @@ HttpBlock::HttpBlock(std::ifstream &File)
 	}
 
 	/**
-	 * @set defaultServerBlock
+	 * @set defaultServerData
 	 * Event 구조체가 들고있어야하는 default server block을 세팅한다.  
 	 * 파싱이 완료된 상태이므로, 접근해서 세팅만하면된다.  
 	 * */
-	this->defaultServerBlock = static_cast<HttpServerBlock *>(this->confData.getServerBlock()[0]);
+	this->defaultServerData = &(static_cast<HttpServerBlock *>(this->confData.getServerBlock()[0])->getServerData());
 }
 
 HttpBlock::~HttpBlock() {
@@ -98,4 +98,4 @@ std::map<int, std::vector<HttpLocationData *> *>& HttpBlock::getLocationDatasByP
 HttpBlock::HttpBlock() {}
 IConfigData* HttpBlock::getConfigData() {return &confData;}
 
-HttpServerBlock *HttpBlock::getDefaultServerBlock() {return this->defaultServerBlock;}
+HttpServerData *HttpBlock::getDefaultServerData() {return this->defaultServerData;}
