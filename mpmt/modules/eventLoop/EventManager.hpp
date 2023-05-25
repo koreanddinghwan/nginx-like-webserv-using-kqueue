817d7d2c184219928935d8bd51ac0ce1035429ec
myukang <myunghwan0421@gmail.com>
2023-05-25 17:22:48 +0900

    feat(eventManager): add event manager

--- mpmt/modules/eventLoop/EventManager.hpp
+++ mpmt/modules/eventLoop/EventManager.hpp
@@ -4,6 +4,14 @@
 #include "Event.hpp"
 #include <map>
 
+/**
+ * @brief EventManager는 Event를 관리하는 클래스이다.
+ * EventManager는 싱글톤으로 구현되어 있다.
+ * EventManager는 Event를 추가하거나 삭제할 수 있다.
+ * EventManager는 Event를 fd를 통해 찾을 수 있다.
+ * EventManager는 Event를 fd를 통해 삭제할 수 있다.
+ * EventManager는 Event를 fd를 통해 존재하는지 확인할 수 있다.
+ */
 class EventManager
 {
 private:
@@ -16,11 +24,10 @@ private:
 public:
 	static EventManager& getInstance();
 
-
 	void addEvent(Event *e);
-
-
 	void deleteEvent(int fd);
+
+
 	/**
 	 * @brief check if client socket exists in event map
 	 *
