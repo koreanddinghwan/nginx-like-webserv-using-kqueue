207328a884ed725a0d230285efd6610a80d696b1
myukang <myunghwan0421@gmail.com>
2023-05-26 21:19:01 +0900

    feat(myukang's reponse): commit

---
+++ mpmt/modules/http/response.hpp
@@ -0,0 +1,20 @@
+#ifndef RESPONSE_HPP
+# define RESPONSE_HPP
+
+#include "../eventLoop/Event.hpp"
+#include "../../interface/IHandler.hpp"
+
+class Response : public IHandler
+{
+	private:
+
+
+
+	public:
+
+
+
+};
+
+
+#endif
