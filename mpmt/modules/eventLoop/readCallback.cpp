49ecd9dbbd1e5d2e08ac9107f0b36ebb23febc04
myukang <myunghwan0421@gmail.com>
2023-05-26 16:03:51 +0900

    feat(comment and debug printer): commit

--- mpmt/modules/eventLoop/readCallback.cpp
+++ mpmt/modules/eventLoop/readCallback.cpp
@@ -8,10 +8,7 @@
 void EventLoop::readCallback(struct kevent *e)
 {
 	std::cout << "\033[32m"; 
-	std::cout<<"read callback"<<std::endl;
-
-	int event_fd = e->ident;
-	std::cout<<"client fd: "<<event_fd<<std::endl;
+	std::cout<<"Read callback"<<std::endl;
 
 	//set event
 	Event *e_udata = static_cast<Event *>(e->udata);
@@ -48,8 +45,9 @@ void EventLoop::readCallback(struct kevent *e)
  */
 void EventLoop::e_serverSocketCallback(struct kevent *e, Event *e_udata)
 {
+	std::cout << "\033[33m"; 
+	std::cout<<"SERVER SOCKET CALLBACK"<<std::endl;
 	//we need to verify http
-
 	if (e_udata->getServerType() == HTTP_SERVER)
 	{
 		//create client socket
@@ -57,7 +55,7 @@ void EventLoop::e_serverSocketCallback(struct kevent *e, Event *e_udata)
 
 		EventManager::getInstance().addEvent(new_udata);
 		t_SocketInfo socketInfo;
-		int sockfd = e_udata->getFd();
+		int sockfd = e_udata->getServerFd();
 		socklen_t cliLen = sizeof(socketInfo.socket_addr);
 		int client_fd;
 
@@ -78,7 +76,7 @@ void EventLoop::e_serverSocketCallback(struct kevent *e, Event *e_udata)
 		/**
 		 * client socket의 fd를 등록
 		 * */
-		new_udata->setFd(client_fd);
+		new_udata->setClientFd(client_fd);
 
 		/**
 		 * client socket의 socketInfo를 등록
@@ -86,14 +84,12 @@ void EventLoop::e_serverSocketCallback(struct kevent *e, Event *e_udata)
 		new_udata->setSocketInfo(socketInfo);
 
 		/**
-		 * client socket의 config 등록
+		 * client socket에 Location data 등록
+		 * 현재 udata의 Event에 이미 port별로 location data가 있음.
+		 * 그대로 복사
+		 * default server data는 생성자에서 초기화됨.
 		 * */
-		new_udata->setConfigData(e_udata->getConfigData());
-
-		/**
-		 * configData는 기존의 configData를 그대로 사용한다.
-		 * port번호별로 이미 등록된 configData가 있기 때문이다.
-		 **/
+		new_udata->setLocationData(e_udata->getLocationData());
 
 		//client socket을 읽기전용으로  kqueue에 등록
 		EV_SET(&(dummyEvent), client_fd, EVFILT_READ, EV_ADD, 0, 0, new_udata);
@@ -105,6 +101,8 @@ void EventLoop::e_serverSocketCallback(struct kevent *e, Event *e_udata)
 
 void EventLoop::e_clientSocketCallback(struct kevent *e, Event *e_udata)
 {
+	std::cout << "\033[34m"; 
+	std::cout<<"client socket callback"<<std::endl;
 	//socket의 readfilter-> EOF flag는 client의 disconnect.
 	if (e->flags == EV_EOF)
 	{
@@ -117,9 +115,9 @@ void EventLoop::e_clientSocketCallback(struct kevent *e, Event *e_udata)
 	if (e_udata->getServerType() == HTTP_SERVER)
 	{
 		//read from client socket
-		int client_fd = e_udata->getFd();
+		int client_fd = e_udata->getClientFd();
 		ssize_t read_len = read(client_fd, HttpServer::getInstance().getHttpBuffer(), 1024);
-
+		HttpServer::getInstance().getStringBuffer().insert(0, HttpServer::getInstance().getHttpBuffer(), read_len);
 		if (read_len == -1)
 		{
 			if (errno == EAGAIN || errno == EWOULDBLOCK)
@@ -133,29 +131,24 @@ void EventLoop::e_clientSocketCallback(struct kevent *e, Event *e_udata)
 			std::cout<<HttpServer::getInstance().getHttpBuffer()<<std::endl;
 			std::cout<<"[[[[[[[CLIENT REQUEST END]]]]]]]]"<<std::endl;
 
-			/**
-			 * Event Manager에서 client socket이 이미 등록되어 있는지 확인(처리완료안된 request가 있는지 확인)
-			 * **/
-			if (EventManager::getInstance().isClientSocketExists(client_fd) == true)
-			{
-				//이미 있는 client socket이라면
-				//pending 상태 가정. (chunked...?)
-				//getEvent(client_fd)->getRequestHandler()->pended();
-			}
-			else
-			{
-				//현재 버퍼에 있는 내용을 가지고 request를 처리한다.
-				e_udata->getRequestHandler()->handle(HttpServer::getInstance().getHttpBuffer());
-				/**
-				 * 파싱=> 
-				 * **/
-			}
+			e_udata->getRequestHandler()->handle(&(HttpServer::getInstance().getStringBuffer()));
+			static_cast<HttpreqHandler *>(e_udata->getRequestHandler())->printReq();
+
+			//handle response by request
 		}
 	}
 }
 
 void EventLoop::e_pipeCallback(struct kevent *e, Event *e_udata)
-{}
+{
+	std::cout << "\033[35m"; 
+	std::cout<<"pipe callback"<<std::endl;
+
+}
 
 void EventLoop::e_fileCallback(struct kevent *e, Event *e_udata)
-{}
+{
+	std::cout << "\033[36m"; 
+	std::cout<<"file callback"<<std::endl;
+
+}
