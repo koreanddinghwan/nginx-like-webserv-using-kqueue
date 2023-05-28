08e0e6642e1070d89db7a36c6096531e02b22d34
Yohan Min <yomin@c4r3s5.42seoul.kr>
2023-05-28 22:32:10 +0900

    back up

--- mpmt/modules/http/test.cpp
+++ mpmt/modules/http/test.cpp
@@ -2,14 +2,23 @@
 #include "iostream"
 #include "sstream"
 #include "HttpResponseInfo.hpp"
+#include "responseHandler.hpp"
+#include "../eventLoop/Event.hpp"
+
 int main(void)
 {
 
 	http_status_msg msg = http_status_msg(404);
 	Response res(404);
+	//Event *event = new Event(HTTP_SERVER);
+	responseHandler *Res = new responseHandler();
+
+	Res->handle(NULL);
+	
 
 	std::cout << "===========" << http_status_codes(404) << "==============" << std::endl;
 	std::cout << "===========" << msg.getStatusMsg() << "==============" << std::endl;
 	std::cout << "===========" << res.getStatusCode() << "==============" << std::endl;
 	std::cout << "===========" << res.getStatusMsg() << "==============" << std::endl;
+
 }
