b3197705f26ec27362253c48520605e711f6dfac
koreanddinghwan <myunghwan0421@gmail.com>
2023-05-28 19:18:31 +0900

    feat(unregister event methods): commit

--- mpmt/modules/eventLoop/registerEvent.cpp
+++ mpmt/modules/eventLoop/registerEvent.cpp
@@ -38,13 +38,6 @@ void EventLoop::registerPipeWriteEvent(Event *e)
 		throw std::runtime_error("Failed to register pipe write with kqueue\n");
 }
 
-/**
- * @brief register file write Event
- *
- * would be used in POST method
- *
- * @param e
- */
 void EventLoop::registerFileWriteEvent(Event *e)
 {
 	EV_SET(&(dummyEvent), e->getFileFd(), EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, e);
@@ -52,23 +45,50 @@ void EventLoop::registerFileWriteEvent(Event *e)
 		throw std::runtime_error("Failed to register file write with kqueue\n");
 }
 
-/**
- * @brief register http exception event
- * first, disable read event to client fd.
- * second, register write event to client socket.
- * and then, when the client socket write event catched in writeCallback
- * @param e
- */
-void EventLoop::registerRequestHttpExceptionEvent(Event *e)
-{
-	//disable client fd read event
-	EV_SET(&this->dummyEvent, e->getClientFd(), EVFILT_READ, EV_DISABLE | EV_DELETE, 0, 0, 0);
-	if (kevent(this->kq_fd, &(this->dummyEvent), 1, NULL, 0, NULL) == -1) 
-		throw std::runtime_error("Failed to register request http exception with kqueue\n");
 
-	//register client fd write event
-	registerClientSocketWriteEvent(e);
+/**
+ * unregister event
+ * */
+void EventLoop::unregisterClientSocketReadEvent(Event *e)
+{
+	//client socket을 읽기전용으로  kqueue에 등록
+	EV_SET(&(dummyEvent), e->getClientFd(), EVFILT_READ, EV_DELETE | EV_DISABLE, 0, 0, e);
+	if (kevent(this->kq_fd, &(dummyEvent), 1, NULL, 0, NULL) == -1) 
+		throw std::runtime_error("Failed to register client socket read with kqueue\n");
 }
 
-void EventLoop::registerResponseHttpExceptionEvent(Event *e)
-{}
+void EventLoop::unregisterPipeReadEvent(Event *e)
+{
+	EV_SET(&(dummyEvent), e->getPipeFd(), EVFILT_READ, EV_DELETE | EV_DISABLE, 0, 0, e);
+	if (kevent(this->kq_fd, &(dummyEvent), 1, NULL, 0, NULL) == -1) 
+		throw std::runtime_error("Failed to register pipe read with kqueue\n");
+}
+
+void EventLoop::unregisterFileReadEvent(Event *e)
+{
+	EV_SET(&(dummyEvent), e->getFileFd(), EVFILT_READ, EV_DELETE | EV_DISABLE, 0, 0, e);
+	if (kevent(this->kq_fd, &(dummyEvent), 1, NULL, 0, NULL) == -1) 
+		throw std::runtime_error("Failed to register file read with kqueue\n");
+}
+
+void EventLoop::unregisterClientSocketWriteEvent(Event *e)
+{
+	//client socket을 읽기전용으로  kqueue에 등록
+	EV_SET(&(dummyEvent), e->getClientFd(), EVFILT_WRITE, EV_DELETE | EV_DISABLE, 0, 0, e);
+	if (kevent(this->kq_fd, &(dummyEvent), 1, NULL, 0, NULL) == -1) 
+		throw std::runtime_error("Failed to register client socket write with kqueue\n");
+}
+
+void EventLoop::unregisterPipeWriteEvent(Event *e)
+{
+	EV_SET(&(dummyEvent), e->getPipeFd(), EVFILT_WRITE, EV_DELETE | EV_DISABLE, 0, 0, e);
+	if (kevent(this->kq_fd, &(dummyEvent), 1, NULL, 0, NULL) == -1) 
+		throw std::runtime_error("Failed to register pipe write with kqueue\n");
+}
+
+void EventLoop::unregisterFileWriteEvent(Event *e)
+{
+	EV_SET(&(dummyEvent), e->getFileFd(), EVFILT_WRITE, EV_DELETE | EV_DISABLE, 0, 0, e);
+	if (kevent(this->kq_fd, &(dummyEvent), 1, NULL, 0, NULL) == -1) 
+		throw std::runtime_error("Failed to register file write with kqueue\n");
+}
