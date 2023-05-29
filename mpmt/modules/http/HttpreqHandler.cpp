6e2ed57db51d785d564ff89e8bc52247059ae0f9
obvoso <autumninmoon@gmail.com>
2023-05-29 20:35:33 +0900

    Feat: add percent Encoding

--- mpmt/modules/http/HttpreqHandler.cpp
+++ mpmt/modules/http/HttpreqHandler.cpp
@@ -280,3 +280,27 @@ int convertHexToDec(std::string line)
 	}
 	return (dec);
 }
+
+//https://stackoverflow.com/questions/154536/encode-decode-urls-in-c
+std::string encodePercentEncoding(const std::string& str)
+{
+  std::ostringstream escaped;
+  escaped.fill('0');
+  escaped << std::hex;
+
+  for (std::string::const_iterator i = str.begin(), n = str.end(); i != n; ++i)
+  {
+    std::string::value_type c = (*i);
+    // Keep alphanumeric and other accepted characters intact
+    if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~')
+    {
+      escaped << c;
+      continue;
+    }
+    // Any other characters are percent-encoded
+    escaped << std::uppercase;
+    escaped << '%' << std::setw(2) << static_cast<int>(static_cast<unsigned char>(c));
+    escaped << std::nouppercase;
+  }
+  return (escaped.str());
+}
