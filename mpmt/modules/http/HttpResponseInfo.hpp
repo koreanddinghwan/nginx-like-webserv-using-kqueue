89b88789be9078b884cd38f40a89134afbb937cd
Yohan Min <yomin@c4r3s7.42seoul.kr>
2023-05-29 16:58:23 +0900

    edit in responseHandler

--- mpmt/modules/http/HttpResponseInfo.hpp
+++ mpmt/modules/http/HttpResponseInfo.hpp
@@ -13,54 +13,25 @@ class Response {
 	//constructor
 	public:
 		Response();
-		Response(const int &status) {
-			setStatusCode(status);
-			setStatusMsg(status);
-		};
+		Response(const int &status);
 
-		Response& operator=(const Response &rhs) {
-		
-		this->setStatusCode(rhs.getStatusCode());
-		this->setStatusMsg(rhs.getStatusCode());
-    	return (*this);
-		}
+		Response& operator=(const Response &rhs);
 	
-		//setHeader
-		//setStatusCode
-		//setBody
-		//getHeader
-		//getStatusCode
-		//getBody
-		void setStatusMsg(int statusCode) {
-			http_status_msg msg = http_status_msg(statusCode);
-			this->_statusMsg = msg.getStatusMsg();
-		}
+		void setStatusMsg(int statusCode);
 
-		void setStatusCode(int statusCode) {
-			this->_statusCode = statusCode;
-		}
+		void setStatusCode(int statusCode);
+		//객체를 받아서 스트림 그자체로 쓸 수도 있음.
 
-		void setHeaders(std::string httpV) {
-			this->_headers << httpV << " " << this->_statusCode << " " << this->_statusMsg << "\r\n";
-			this->_headers << "Content-Type: " << "text/html" << "\r\n";
-			if (this->_statusCode == 301)
-				this->_headers << "Location: " << "probly file Path Variable added";
-			this->_body.str() != "" ? this->_headers << "Content-Length: " << this->_body.str().length() << "\r\n\r\n" : this->_headers << "\r\n\r\n";
-		}	
-		void setBody(std::string body) {
-			this->_body << body;
-		}
+		void setHeaders(std::string httpV);	
+		void setBody(std::string body);
+		void setBuf();
 
-		void setBuf(){
-			this->_buf << getHeader() << getBody();
-		}
+		std::string getBody() const;
+		std::string getHeader() const;
+		std::string getBuf() const;
 
-		std::string getBody() const { return this->_body.str();}
-		std::string getHeader() const { return this->_headers.str();}
-		std::string getBuf() const {return this->_buf.str(); }
-
-		std::string getStatusMsg() const { return this->_statusMsg; }
-		int getStatusCode() const { return this->_statusCode; }
+		std::string getStatusMsg() const;
+		int getStatusCode() const;
 		
 	//destoryer
 	protected:
---
+++ mpmt/modules/http/responseHandler.cpp
@@ -0,0 +1,58 @@
+#include "responseHandler.hpp"
+
+
+responseHandler::responseHandler() {
+};
+
+responseHandler::responseHandler(const int &status) {
+	this->_res = new Response(status);
+};
+
+responseHandler& responseHandler::operator=(const responseHandler &rhs) {
+	this->_res = rhs._res;
+    return (*this);
+};
+
+responseHandler::~responseHandler() {
+	delete this->_res;
+};
+
+void responseHandler::setRes(const int statusCode) {
+	this->_res = new Response(statusCode);
+};
+
+void responseHandler::setResBody(std::string body) const { 
+	this->_res->setBody(body); 
+};
+
+void responseHandler::setResHeader(std::string HttpV) const { 
+	this->_res->setHeaders(HttpV); 
+};
+
+void responseHandler::setResBuf() const {
+	this->_res->setBuf(); 
+};
+
+std::string responseHandler::getResBody() const {
+	return this->_res->getBody();
+};
+
+std::string responseHandler::getResHeader() const {
+	return this->_res->getHeader();
+};
+
+std::string responseHandler::getResBuf() const {
+	return this->_res->getBuf();
+};
+
+void *responseHandler::handle(void *event) {
+	Event *e = static_cast<Event *>(event);
+
+	this->setRes(302);
+	this->setResBody("bodybody");
+	std::cout << "=====================\n" << this->getResBody() << "=====================\n" << std::endl;
+	this->setResHeader(HTTPV11);
+	std::cout << "=====================\n" << this->getResHeader() << "=====================\n" << std::endl;
+	this->setResBuf();
+	std::cout << "=====================\n" << this->getResBuf() << "\n=====================" << std::endl;
+}
