6f50a9a4cdc6d1f6f3a2d39cb54bc5615ac53a57
Yohan Min <yomin@c4r3s7.42seoul.kr>
2023-05-29 21:29:23 +0900

    fixing setLoaction, and add getResStatusCode and Msg

--- mpmt/modules/http/test.cpp
+++ mpmt/modules/http/test.cpp
@@ -10,8 +10,32 @@ int main(void)
 
 	http_status_msg msg = http_status_msg(404);
 	Response res(404);
+	std::ifstream test ("index.html", std::ios_base::in);
+	std::string s;
+	responseHandler *Res = new responseHandler(400);
+	std::cout << "========" << Res->getResStatusCode() << std::endl;
+	std::cout << "========" << Res->getResStatusMsg() << std::endl;
+
+	if (test.is_open()) {
+		test.seekg(0, std::ios::end);
+		int size = test.tellg();
+		s.resize(size);
+		test.seekg(0, std::ios::end);
+		test.read(&s[0], size);
+	}
+	int i = 0;
+	std::cout<< s << std::endl;
+	while (i < 3) {
+		Res->setResBody(s);
+		Res->setResBody("======================================\n");
+		i++;
+	}
+	std::cout << "===============" << Res->getResBody().capacity() << "================\n" <<std::endl;
+	std::cout << "===============" << Res->getResBody().size() << "================\n" <<std::endl;
+	Res->setResStatusCode(301);
+	Res->setResStatusMsg(301);
+
 	//Event *event = new Event(HTTP_SERVER);
-	responseHandler *Res = new responseHandler();
 
 	Res->handle(NULL);
 }
