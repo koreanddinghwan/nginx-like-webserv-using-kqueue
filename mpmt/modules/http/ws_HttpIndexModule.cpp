79c43045019ab27bff35924421378b0010ff9766
myukang <myunghwan0421@gmail.com>
2023-05-30 21:48:11 +0900

    feat(index module): commit

--- mpmt/modules/http/ws_HttpIndexModule.cpp
+++ mpmt/modules/http/ws_HttpIndexModule.cpp
@@ -7,41 +7,56 @@ bool ws_HttpIndexModule::processEvent(Event *e)
 	{
 		e->openFileName = e->locationData->getIndex().at(i);
 		//if index file start with /, absolute path
+		std::cout<<"index file name: "<<e->openFileName<<std::endl;
 		if (e->openFileName[0] == '/')
 		{
+			std::cout<<"absolute path"<<std::endl;
 			//만약 있으면
-			if (stat(e->openFileName.c_str(), &e->statBuf) == 0)
+			if ((stat(e->openFileName.c_str(), &e->statBuf) == 0) && 
+					(e->file_fd = open(e->openFileName.c_str(), O_RDONLY)) != -1)
 			{
-				//open file and file read event
-				e->file_fd = open(e->openFileName.c_str(), O_RDONLY);
-				if (e->file_fd == -1)
-				{
-					e->setStatusCode(404);
-					return false;
-				}
-
 				//set nonblock
 				if (fcntl(e->file_fd, F_SETFL, O_NONBLOCK) == -1)
 				{
-					e->setStatusCode(404);
+					e->setStatusCode(500);
 					return false;
 				}
 				e->setStatusCode(200);
 				return true;
+			}
+			else
+			{
+				if (e->locationData->getAutoIndex() == false)
+				{
+					e->setStatusCode(404);
+					return false;
+				}
 			}
 		}
 		else
 		{
-			//else, relative path
-			e->setRoute(e->getDir() + "/" + e->openFileName);
-			if (stat(e->getRoute().c_str(), &e->statBuf) == 0)
+			std::cout<<"relative path"<<std::endl;
+			std::string tmp = e->locationData->getRoot() + "/" + e->openFileName;
+
+			if ((stat(tmp.c_str(), &e->statBuf) == 0) && 
+						(e->file_fd = open(tmp.c_str(), O_RDONLY)) != -1)
 			{
+				if (fcntl(e->file_fd, F_SETFL, O_NONBLOCK) == -1)
+				{
+					e->setStatusCode(500);
+					return false;
+				}
 				e->setStatusCode(200);
-				//set event
 				return true;
+			}
+			else
+			{
+				if (e->locationData->getAutoIndex() == false)
+				{
+					e->setStatusCode(404);
+					return false;
+				}
 			}
 		}
 	}
-	e->setStatusCode(404);
-	return false;
 }
