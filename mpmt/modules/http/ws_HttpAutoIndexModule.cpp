5807e8b6c91913fd7ee0818be133d4b872f880b4
myukang <myunghwan0421@gmail.com>
2023-06-02 04:29:04 +0900

    feat(auto index module): commit

--- mpmt/modules/http/ws_HttpAutoIndexModule.cpp
+++ mpmt/modules/http/ws_HttpAutoIndexModule.cpp
@@ -1,7 +1,10 @@
 #include "./ws_HttpAutoIndexModule.hpp"
 #include <dirent.h>
+#include <stdlib.h>
 #include <string>
+#include <sys/stat.h>
 #include <vector>
+#include "HttpReqHandler.hpp"
 #include "responseHandler.hpp"
 
 bool ws_HttpAutoIndexModule::processEvent(Event *e)
@@ -17,11 +20,26 @@ bool ws_HttpAutoIndexModule::processEvent(Event *e)
 	if (dir)
 	{
 		struct dirent *ent;
+		std::string path;
 
 		//read dir
 		while ((ent = readdir(dir)) != NULL)
 		{
-			list.push_back(ent->d_name);
+			if (ent->d_name[0] == '.')
+			{
+				std::string tmp = ent->d_name;
+				list.push_back(tmp + "/");
+			}
+			else {
+				path = e->getResource() + ent->d_name;
+				if (path.front() == '/')
+					path = path.substr(1);
+				stat((e->getRoute() + ent->d_name).c_str(), &(e->statBuf));
+				if (S_ISDIR(e->statBuf.st_mode))
+					list.push_back(path + std::string("/"));
+				else
+					list.push_back(path);
+			}
 		}
 		closedir(dir);
 
@@ -29,7 +47,8 @@ bool ws_HttpAutoIndexModule::processEvent(Event *e)
 		 * make html response body
 		 * */
 		responseHandler *resHandler = static_cast<responseHandler *>(e->getResponseHandler());
-
+		HttpreqHandler *reqHandler = static_cast<HttpreqHandler *>(e->getRequestHandler());
+		char buf[6];
 
 		resHandler->setResBody("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\" /><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><title>");
 		resHandler->setResBody(e->getRoute());
@@ -38,17 +57,20 @@ bool ws_HttpAutoIndexModule::processEvent(Event *e)
 		resHandler->setResBody("</h1><hr />");
 		for (int i = 0; i < list.size(); i++)
 		{
-			resHandler->setResBody("<a href=./");
+			resHandler->setResBody("<a href=http://");
+			resHandler->setResBody(reqHandler->getRequestInfo().host);
+			resHandler->setResBody(":");
+			sprintf(buf, "%d/", e->locationData->getListen());
+			resHandler->setResBody(buf);
 			resHandler->setResBody(list[i]);
-			resHandler->setResBody("\">");
+			resHandler->setResBody(">");
 			resHandler->setResBody(list[i]);
 			resHandler->setResBody("</a><br />");
 		}
 		resHandler->setResBody("<hr /></body></html>");
 		e->setStatusCode(200);
+		return true;
 	}
-
-
 	e->setStatusCode(404);
 	return false;
 }
