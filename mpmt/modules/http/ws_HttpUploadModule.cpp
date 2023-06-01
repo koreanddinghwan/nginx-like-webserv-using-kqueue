c36b1bbae1ff899c00a272da3ea0b059eba6d9c6
myukang <myunghwan0421@gmail.com>
2023-06-01 14:55:31 +0900

    feat(upload, index module 수정): commit

--- mpmt/modules/http/ws_HttpUploadModule.cpp
+++ mpmt/modules/http/ws_HttpUploadModule.cpp
@@ -6,18 +6,31 @@ bool ws_HttpUploadModule::processEvent(Event *e)
 {
 	std::cout<<"uploda store: "<<e->locationData->getUploadStore()<<std::endl;
 	/**
-	 * @TODO get file name from request
+	 * POST method do not allow create directory
 	 * */
-	std::string fileName = "testFile";
-
-	//create file
-	std::string filePath = e->locationData->getRoot() + "/" + e->locationData->getUploadStore() + "/" + fileName;
-
-	if((e->file_fd = open(filePath.c_str(), O_CREAT | O_WRONLY | O_TRUNC, 0644)) == -1)
+	if (e->getResource().back() == '/')
+	{
+		// 400? 403?
+		std::cout<<"do not allow dir creation"<<std::endl;
+		e->setStatusCode(403);
 		return false;
+	}
+
+	std::string filePath = e->locationData->getRoot() + "/" + e->locationData->getUploadStore() + "/"+ e->getResource();
+	std::cout<<"upload file path: "<<filePath<<std::endl;
+	if((e->file_fd = open(filePath.c_str(), O_CREAT | O_WRONLY | O_TRUNC, 0644)) == -1)
+	{
+		std::cout<<"cannot open file"<<std::endl;
+		e->setStatusCode(500);
+		return false;
+	}
 
 	if ((fcntl(e->file_fd, F_SETFL, O_NONBLOCK)) == -1)
+	{
+		std::cout<<"cannot set nonblock"<<std::endl;
+		e->setStatusCode(500);
 		return false;
-
+	}
+	e->setStatusCode(201);
 	return true;
 }
