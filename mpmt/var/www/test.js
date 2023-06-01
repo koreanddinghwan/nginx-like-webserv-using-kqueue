7f7ad4fb68145f4743898ac53b869e421980b995
myukang <myunghwan0421@gmail.com>
2023-06-01 16:05:12 +0900

    chore: config file
    8080 포트로 사용하세요

--- mpmt/var/www/test.js
+++ mpmt/var/www/test.js
@@ -1,5 +1,15 @@
 const UploadServer = "http://localhost:80/upload/";
 
+const test1 = async () => {
+  const postedResult = await fetch(UploadServer, {
+    method: "POST",
+  });
+};
+
+const test1btn = document.getElementById("test1");
+
+test1btn.addEventListener("click", test1);
+
 const test = async (event) => {
   console.log("test");
   console.log(event);
