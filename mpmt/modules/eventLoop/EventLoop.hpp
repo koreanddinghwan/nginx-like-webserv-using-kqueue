345599df94a655a08f449135a3cadd47fc1ee72a
myukang <myunghwan0421@gmail.com>
2023-06-02 12:31:01 +0900

    refactor(setresponse 함수 분리): commit
    1. errorpage internal redirection 위함.
    
    @ref https://nginx.org/en/docs/http/ngx_http_core_module.html#error_page
    
    Event 내부에 변수 하나 플래그 두고, setResponse의 일부 함수만
    호출하게끔해야할듯.

--- mpmt/modules/eventLoop/EventLoop.hpp
+++ mpmt/modules/eventLoop/EventLoop.hpp
@@ -4,6 +4,7 @@
 #include "../../factory/MethodFactory.hpp"
 #include "../../exceptions/httpException.hpp"
 #include <cstdlib>
+#include <exception>
 #include <sys/_types/_socklen_t.h>
 #include <sys/_types/_ssize_t.h>
 #include <sys/socket.h>
@@ -17,7 +18,9 @@
 #include "Event.hpp"
 #include "../http/responseHandler.hpp"
 #include "../http/HttpreqHandler.hpp"
-
+#include "../http/ws_HttpIndexModule.hpp"
+#include "../http/ws_HttpAutoIndexModule.hpp"
+#include "../http/ws_HttpUploadModule.hpp"
 /**
  * @brief singleton eventloop
  */
@@ -87,9 +90,15 @@ private:
 	void unregisterPipeWriteEvent(Event *e);
 	void unregisterFileWriteEvent(Event *e);
 
-	int getLongestPrefixMatchScore(const std::string& location, const std::string& requestPath);
+	/**
+	 * response setter
+	 * */
 	void setHttpResponse(Event *e);
 	bool processCgi(Event *e);
+	
+	void ws_method_GET(Event *e) throw (std::exception);
+	void ws_method_POST(Event *e) throw (std::exception);
+	void ws_method_DELETE(Event *e) throw (std::exception);
 };
 
 
---
+++ mpmt/modules/eventLoop/methods.cpp
@@ -0,0 +1,114 @@
+#include "EventLoop.hpp"
+#include <exception>
+
+void EventLoop::ws_method_GET(Event *e) throw (std::exception)
+{
+	std::cout<<"method is GET or HEAD"<<std::endl;
+	/**
+	 * 1. check if the requested resource is directory
+	 * */
+	if (e->getRoute().back() == '/')
+	{
+		// process directory
+		// 1. first, check index
+		if (!(e->locationData->getIndex().empty()))
+		{
+			std::cout<<"check index setted"<<std::endl;
+			std::cout<<"|||||||||"<<"index exists"<<"|||||||||"<<std::endl;
+			if (ws_HttpIndexModule::processEvent(e) == false)
+				throw std::exception();
+			else
+			{
+				//pipelining do not used in these days
+				//https://en.wikipedia.org/wiki/HTTP_pipelining
+				unregisterClientSocketReadEvent(e);
+				registerFileReadEvent(e);
+				return ;
+			}
+		}
+		else if (e->locationData->getAutoIndex())
+		{
+			std::cout<<"check autoindex setted"<<std::endl;
+			if (ws_HttpAutoIndexModule::processEvent(e) == false)
+				throw std::exception();
+			else
+			{
+				unregisterClientSocketReadEvent(e);
+				registerClientSocketWriteEvent(e);
+				return ;
+			}
+		}
+		else {
+			std::cout<<"route endwith / but index not setted"<<std::endl;
+			e->setStatusCode(404);
+			throw std::exception();
+		}
+	}
+	else
+	{
+	/**
+	 * 2. resource is file
+	 * */
+		std::cout<<"resource is file"<<std::endl;
+		
+		if ((stat(e->getRoute().c_str(), &e->statBuf) == 0) &&
+				(e->file_fd = open(e->getRoute().c_str(), O_RDONLY)) != -1)
+		{
+			if (fcntl(e->file_fd, F_SETFL, O_NONBLOCK) == -1)
+			{
+				std::cout<<"fcntl error"<<std::endl;
+				e->setStatusCode(500);
+				throw std::exception();
+			}
+			
+			if (e->statBuf.st_size == 0)
+			{
+				std::cout<<"file size is 0"<<std::endl;
+				e->setStatusCode(204);
+				throw std::exception();
+			}
+			e->setStatusCode(200);
+			unregisterClientSocketReadEvent(e);
+			registerFileReadEvent(e);
+			return ;
+		}
+		else
+		{
+			std::cout<<"file open error"<<std::endl;
+			e->setStatusCode(404);
+			throw std::exception();
+		}
+	}
+}
+
+void EventLoop::ws_method_POST(Event *e) throw(std::exception)
+{
+	std::cout<<"method is POST"<<std::endl;
+	if (!e->locationData->getUploadStore().empty())
+	{
+		/**
+		 * upload store is setted
+		 * */
+		if (ws_HttpUploadModule::processEvent(e) == false)
+			throw std::exception();
+		unregisterClientSocketReadEvent(e);
+		registerFileWriteEvent(e);
+		return ;
+	}
+	else
+	{
+		/**
+		 * Upload store is not setted
+		 * */
+		e->setStatusCode(404);
+		throw std::exception();
+	}
+}
+
+void ws_method_DELETE(Event *e) throw (std::exception)
+{
+	std::cout<<"method is DELETE"<<std::endl;
+	/**
+	 * check if the resource exists
+	 * */
+}
