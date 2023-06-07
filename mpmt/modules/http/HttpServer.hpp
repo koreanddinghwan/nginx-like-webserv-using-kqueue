f93142ad2e37794aae36c978f7bac6cc92e392e3
myukang <myunghwan0421@gmail.com>
2023-06-07 13:16:37 +0900

    feat: http buffer size

--- mpmt/modules/http/HttpServer.hpp
+++ mpmt/modules/http/HttpServer.hpp
@@ -17,7 +17,7 @@
 #include <netinet/in.h>
 #include <stdexcept>
 #include <sys/event.h>
-# define HTTPBUFFER_SIZE 8192
+# define HTTPBUFFER_SIZE 65536
 
 class HttpServer : public IServer {
 private:
