ab645a9c6cab9c3e9c0f207760ef089db165fd85
koreanddinghwan <myunghwan0421@gmail.com>
2023-06-12 22:32:51 +0900

    feat: logger

--- modules/eventLoop/EventLoop.cpp
+++ modules/eventLoop/EventLoop.cpp
@@ -7,6 +7,9 @@
 #include <sys/_types/_uintptr_t.h>
 #include <sys/event.h>
 
+void printEvent(void *e);
+
+
 EventLoop& EventLoop::getInstance() {
 	static EventLoop instance;
 	return instance;
@@ -34,6 +37,7 @@ void EventLoop::initEventLoop()
 
 		for (int i = 0; i < nevents; i++)
 		{
+			printEvent(events[i].udata);
 			try {
 				if (events[i].filter == EVFILT_READ)
 					readCallback(events + i);
@@ -54,6 +58,37 @@ void EventLoop::initServerEvents()
 	{
 		if (kevent(this->kq_fd, &(*it), 1, NULL, 0, NULL) == -1) 
 			throw std::runtime_error("Failed to register socket event\n");
+	}
+}
+
+
+void printEvent(void *a)
+{
+	Event *e = static_cast<Event *>(a);
+	HttpreqHandler *reqHandler = static_cast<HttpreqHandler *>(e->getRequestHandler());
+	std::cout<<"\033[33m"<<"===================="<<std::endl;
+	std::cout<<"Event Information"<<std::endl;
+	std::cout<<"===================="<<std::endl;
+	if (e->getServerType() == HTTP_SERVER)
+		std::cout<<"Server Type : HTTP_SERVER"<<std::endl;
+	if (e->getEventType() == E_SERVER_SOCKET)
+		std::cout<<"Event Type : E_SERVER_SOCKET"<<std::endl;
+	else if (e->getEventType() == E_CLIENT_SOCKET)
+		std::cout<<"Event Type : E_CLIENT_SOCKET"<<std::endl;
+	else if (e->getEventType() == E_FILE)
+		std::cout<<"Event Type : E_FILE"<<std::endl;
+	else if (e->getEventType() == E_PIPE)
+		std::cout<<"Event Type : E_PIPE"<<std::endl;
+	if (e->getEventType() != E_SERVER_SOCKET)
+	{
+		std::cout<<"Server Socket fd : "<<e->getServerFd()<<std::endl;
+		std::cout<<"Client Socket fd : "<<e->getClientFd()<<std::endl;
+		std::cout<<"File fd : "<<e->file_fd<<std::endl;
+		std::cout<<"Status Code : "<<e->getStatusCode()<<std::endl;
+		std::cout<<"Route : "<<e->getRoute()<<std::endl;
+		std::cout<<"Uri : "<<reqHandler->getRequestInfo().path<<std::endl;
+		std::cout<<"Method : "<<reqHandler->getRequestInfo().method<<std::endl;
+		std::cout<<"===================="<<std::endl;
 	}
 }
 
